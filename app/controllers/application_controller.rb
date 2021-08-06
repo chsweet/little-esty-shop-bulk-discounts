68a8a1f0af3b8ec4338bef51690cedbfbdc303f7
Carina Sweet <carina.sweet06@gmail.com>
2021-08-06 16:04:22 -0600

    Remove little esty shop API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :set_layout_variables
 
   def set_layout_variables
-    @repo = GithubRepo.new(GithubService.repo_info)
-    @repo_contributors = GithubContributor.new(GithubService.contributor_info)
-    @repo_pulls = GithubPulls.new(GithubService.pulls_info)
+    # @repo = GithubRepo.new(GithubService.repo_info)
+    # @repo_contributors = GithubContributor.new(GithubService.contributor_info)
+    # @repo_pulls = GithubPulls.new(GithubService.pulls_info)
   end
 
   private
