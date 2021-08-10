594f59f79bec5f1d4e7311d928aec57743a6b77b
Carina Sweet <carina.sweet06@gmail.com>
2021-08-10 14:42:57 -0600

    Add bulk discount seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+BulkDiscount.destroy_all
+
+BulkDiscount.create!(percentage_discount: 20, quantity: 10, merchant_id: 5)
+BulkDiscount.create!(percentage_discount: 15, quantity: 15, merchant_id: 5)
+BulkDiscount.create!(percentage_discount: 25, quantity: 25, merchant_id: 7)
+BulkDiscount.create!(percentage_discount: 20, quantity: 10, merchant_id: 7)
+BulkDiscount.create!(percentage_discount: 20, quantity: 10, merchant_id: 8)
+BulkDiscount.create!(percentage_discount: 15, quantity: 10, merchant_id: 8)
